602200ade23d596252e02385da65297f19bbe3cd
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-09-01 15:20:25 -0300

    Converting string data to tensor

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,9 @@
 #include <string>
 #include <fstream>
 #include <sstream>
+#include <iomanip>
 #include <stdexcept>
+#include <torch/torch.h>
 
 std::string readDataFromFile(const std::string &filename) {
     std::ifstream file(filename);    
@@ -15,6 +17,20 @@ std::string readDataFromFile(const std::string &filename) {
     return std::string((std::istreambuf_iterator<char>(file)), std::istreambuf_iterator<char>());
 }
 
+torch::Tensor fromStringToTensor(const std::string &data) {
+    std::vector<double> dataVector = {};
+    std::stringstream stream(data);
+    std::string tmp;
+
+    while (stream >> tmp)
+        dataVector.push_back(std::stod(tmp));
+
+    auto opts = torch::TensorOptions().dtype(torch::kDouble);
+    torch::Tensor output = torch::from_blob(dataVector.data(), {int(dataVector.size())/4, 4}, opts).to(torch::kDouble);
+    
+    return output;
+}
+
 int main(int argc, const char** argv) {
     printf("Pytorch and Libldsc integration\n");
     printf("Read file local file...\n");
@@ -22,7 +38,9 @@ int main(int argc, const char** argv) {
     std::string filename = "/home/joao/Documentos/dev/C++/test-pytorch/data/training.dat";
     std::string trainingData = readDataFromFile(filename);
 
-    printf("Data: %s", trainingData.c_str());
+    torch::Tensor tensorData = fromStringToTensor(trainingData);
+
+    std::cout << tensorData << std::endl;
 
     return 0;
 }
