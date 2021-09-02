3d53198e66ec4f6b4c211f646873f605af6650cf
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-09-02 10:30:21 -0300

    Traing Function

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,46 @@
 #include "RockImageRGBNet/RockImageRGBNet.hpp"
 #include "utils/utils.hpp"
 
+template<typename DataLoader>
+void executeTraining(
+    int epoch,
+    std::shared_ptr<RockImageRGBNet> model,
+    DataLoader& data_loader,
+    torch::optim::Optimizer &optimizer,
+    int datasetSize
+) {
+    model->train();
+    size_t batchIndex = 0;
+    
+    for(auto &batch : data_loader)
+    {
+        auto data = batch.data;
+        auto target = batch.target.squeeze();
+
+        data = data.to(torch::kF32);
+        target = target.to(torch::kInt64);
+
+        optimizer.zero_grad();
+
+        auto output = model->forward(data);
+        auto loss = torch::nll_loss(output, target);
+
+        loss.backward();
+        optimizer.step();
+
+        if (batchIndex++ % 10 == 0) {
+            std::printf(
+                "\rEpoch: %d [%5ld/%5d] Loss: %f",
+                epoch,
+                batchIndex * batch.data.size(0),
+                datasetSize,
+                loss.template item<float>()
+            );
+        }
+        
+    }
+}
+
 int main(int argc, const char** argv) {
     double lr = 0.04;
     int batch_size = 5;
@@ -30,33 +70,40 @@ int main(int argc, const char** argv) {
     auto net = std::make_shared<RockImageRGBNet>();
     torch::optim::SGD optimizer(net->parameters(), /*lr = */lr);
 
-    auto dataset_size = dataset.size().value();
+    auto datasetSize = dataset.size().value();
     for (int epoch = 0; epoch <= 1000; epoch++)
     {
-        for (auto &batch : *dataLoader)
-        {
-            auto data = batch.data;
-            auto target = batch.target.squeeze();
+        executeTraining(
+            epoch,
+            net,
+            *dataLoader,
+            optimizer,
+            datasetSize
+        );
+        // for (auto &batch : *dataLoader)
+        // {
+        //     auto data = batch.data;
+        //     auto target = batch.target.squeeze();
 
-            data = data.to(torch::kF32);
-            target = target.to(torch::kInt64);
+        //     data = data.to(torch::kF32);
+        //     target = target.to(torch::kInt64);
 
-            optimizer.zero_grad();
+        //     optimizer.zero_grad();
 
-            auto output = net->forward(data);
-            auto loss = torch::nll_loss(output, target);
+        //     auto output = net->forward(data);
+        //     auto loss = torch::nll_loss(output, target);
 
-            loss.backward();
-            optimizer.step();
+        //     loss.backward();
+        //     optimizer.step();
 
-            std::cout 
-                << "Train Epoch: "
-                << epoch
-                << " Loss: "
-                << loss.item<float>()
-                << std::endl;
+        //     std::cout 
+        //         << "Train Epoch: "
+        //         << epoch
+        //         << " Loss: "
+        //         << loss.item<float>()
+        //         << std::endl;
             
-        }
+        // }
     }
 
     torch::save(net, "/home/joao/Documentos/dev/C++/test-pytorch/data/model.dat");    
